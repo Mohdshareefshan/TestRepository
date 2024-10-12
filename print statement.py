f51909691ab0f9d81911a254072770121ecb5c97
Mohdshareefshan <mohdshareefshan@gmail.com>
2024-10-12 03:03:32 -0700

    Create print statement.py

---
+++ print statement.py
@@ -0,0 +1,79 @@
+#Basic String Output 
+print("Hello, World!") 
+
+#Simple Math Inside Print
+print(5 + 10)
+print(21+32) 
+
+ #Multiple Arguments in Print 
+print("Python", "is", "fun!")
+print('hacking', 'is', 'challenging$')
+
+# Print with Escape Characters 
+print("She said, \"Hello!\"") 
+print('He replayed, \'hi!\'') 
+
+ #Newline Character
+print("First line\nSecond line") 
+print ('mohd shareef\nshan')
+
+ #Tab Character 
+print("Item 1\tItem 2\tItem 3") 
+print('Mouse\tKeyboard\tCpu') 
+ 
+# Print Multiple Data Types
+print("The result is", 42) 
+print("The result is", 95) 
+
+# Print a Single Number 
+print(21363)
+print(412536)
+
+#Print a Decimal Number 
+print(1.2356)
+print(5.36236)
+
+#Print Special Characters
+print('dollor sign: $') 
+print ('percentage sign: %')
+
+#Using the sep Parameter
+print("apple", "banana", "cherry", sep=',')
+print('PUBG', 'COD', 'FREEFIRE', sep='-')
+
+#Using the end Parameter 
+print("Hello", end=", ") 
+print("world!")
+
+#Print a Repeated String 
+print('hackingskills' *4,())
+print('python'*3)
+
+#Print a Unicode Character
+print("\u2764") # Prints a heart symbol
+
+#Print Empty Line 
+print()
+
+
+#Concatenating Strings Inside
+print("Hello " + "World!")
+print("HACKING " + "World!")
+
+#Print Boolean Values 
+print(True) 
+print(False) 
+
+#Printing Exponential 
+print(1.2e3)
+print(1.2e10000) 
+print(1.2e9)
+
+# Print Multiline Text Using Triple 
+print("""TODAY IS VERY GOOD DAY""")
+
+# Print Using Escaped 
+print('TODAY CLASS IS VERRY GOOD:\\')
+print('brillant class:\\')
+
+
